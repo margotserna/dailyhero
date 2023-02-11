0e6668e30a4f16e32966e49383d019d918896cd4
Margot <margot.serna@hotmail.com>
2023-02-11 21:48:29 +0100

    ADD super heroes list

---
+++ src/components/SuperHeroList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="super-hero-list">
+    <h1>Liste</h1>
+    <div v-for="(superhero, index) in superheroes" v-bind:key="index">
+        <SuperHeroItem :pseudo="superhero.pseudo" :ville="superhero.ville" :imageUrl="superhero.imageUrl"></SuperHeroItem>    
+    </div>
+  </div>
+</template>
+
+<script>
+import SuperHeroItem from "./SuperHeroItem.vue";
+import axios from 'axios';
+
+export default {
+  name: 'SuperHeroList',
+  props: {
+  }, 
+  components: {
+    SuperHeroItem:SuperHeroItem
+  },
+  mounted () {
+    axios
+      .get('https://eu-west-2.aws.data.mongodb-api.com/app/dailyhero-cypmd/endpoint/users')
+      .then(response => (this.superheroes = response.data))
+  }, 
+  data () {
+    return {
+        superheroes: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SuperHeroesView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    <SuperHeroList/>
+  </div>
+</template>
+
+<script>
+import SuperHeroList from '../components/SuperHeroList.vue'
+
+export default {
+  name: "SuperHeroView",
+  components: {
+    SuperHeroList
+  }
+};
+</script>
